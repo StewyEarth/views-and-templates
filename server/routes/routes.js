f3e0b9b0be3916e4761b946eae813b241b12d35c
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-05-27 13:16:32 +0200

    Added salebanner and variables in routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,12 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
-      res.render('home');
+      res.render('home', { "title": "Home" });
    });
-   app.get('/product', (req, res, next) => {
-      res.render('product');
+   app.get('/products', (req, res, next) => {
+      res.render('products', { "title": "Products", "products": [{ "name": "Watch", "picture": "/img/products/product1.jpg" }, { "name": "Headphones", "picture": "/img/products/product2.jpg" }, { "name": "Delete" }] });
    });
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', { "title": "Contact" });
+      res.send();
    });
 };
