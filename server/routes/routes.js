0ed3de7909d94fa4fc04798bf9f6ab49eeb08dfd
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-05-27 10:16:18 +0200

    Got basic stuff to work

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,4 +2,10 @@ module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home');
    });
+   app.get('/product', (req, res, next) => {
+      res.render('product');
+   });
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
 };
