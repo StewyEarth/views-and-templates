5a65b2e3f122705b2a2d3af1f8c89d623c30706f
Daniel Therkildsen <dtherkildsen@hotmail.com>
2019-05-28 08:42:36 +0200

    Added active page thing

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,11 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
-      res.render('home', { "title": "Home" });
+      res.render('home', { "title": "Home","page":"home" });
    });
    app.get('/products', (req, res, next) => {
-      res.render('products', { "title": "Products", "products": [{ "name": "Watch", "picture": "/img/products/product1.jpg" }, { "name": "Headphones", "picture": "/img/products/product2.jpg" }, { "name": "Delete" }] });
+      res.render('products', { "title": "Products","page":"products", "products": [{ "name": "Watch", "picture": "/img/products/product1.jpg" }, { "name": "Headphones", "picture": "/img/products/product2.jpg" }, { "name": "Delete" }] });
    });
    app.get('/contact', (req, res, next) => {
-      res.render('contact', { "title": "Contact" });
-      res.send();
+      res.render('contact', { "title": "Contact","page":"contact"});
    });
 };
